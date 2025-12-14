71a24050630e81d4054abbb76fbeba024bca29e6
wasabina67 <wasabina67@gmail.com>
2025-12-15 02:04:37 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  const formattedTime = time.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  })
+
+  const formattedDate = time.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+
+  return (
+    <div className="app">
+      <div className="dog-container">
+        <img
+          src="https://cdn2.thedogapi.com/images/ry1kWe5VQ_1280.jpg"
+          alt="dog"
+          className="dog-image"
+        />
+        <div className="clock">
+          <div className="date">{formattedDate}</div>
+          <div className="time">{formattedTime}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
