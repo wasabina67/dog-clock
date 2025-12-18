dd08a19bc5d945c2ca302e36da9bf4b35fac0977
gh-actions <gh-actions@users.noreply.github.com>
2025-12-18 10:07:11 +0000

    build

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
     <div className="app">
       <div className="dog-container">
         <img
-          src="https://cdn2.thedogapi.com/images/dcFcTU2vY.jpg"
+          src="https://cdn2.thedogapi.com/images/aJ6N5Kf-l.jpg"
           alt="dog"
           className="dog-image"
         />
